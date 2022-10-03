625f684b8364c95b68c4f3ab82344bbc1e8b9010
ccfries <cfries@google.com>
2022-10-03 12:46:17 +0000

    gta4l-common: Set VENDOR_SECURITY_PATCH To PLATFORM
    Set the VENDOR_SECURITY_PATCH level to match the platform security patch level.
    These are functionally the same.
    
    Signed-off-by: kibria5 <mdkibria687@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -208,7 +208,7 @@ BOARD_ROOT_EXTRA_SYMLINKS += /vendor/firmware_mnt:/firmware
 BOARD_ROOT_EXTRA_SYMLINKS += /vendor/bt_firmware:/bt_firmware
 
 # Security Patch Level
-VENDOR_SECURITY_PATCH := 2024-06-01
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
