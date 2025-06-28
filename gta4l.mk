1cd09c5b89e967e529455e4b0a3cc3c0499d1d68
Bruno Martins <bgcngm@gmail.com>
2025-06-28 12:45:00 +0530

    gta4l-common: Migrate Lineage Health to soong_config_set
    Change-Id: I2eff39e83b9673aa2989d05de65c646241ba61da

--- gta4l.mk
+++ gta4l.mk
@@ -174,6 +174,14 @@ $(call soong_config_set,samsungVars,target_keymaster4_library,//vendor/samsung/g
 # Lineage Health
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
+
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/batt_slate_mode)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
+$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+$(call soong_config_set,lineage_health,charging_control_supports_toggle,true)
+$(call soong_config_set,lineage_health,charging_control_supports_deadline,false)
+
 # Logging
 SPAMMY_LOG_TAGS := \
     MiStcImpl \
